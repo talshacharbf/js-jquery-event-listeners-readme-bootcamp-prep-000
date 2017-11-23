e1bd64c4c8bd654289e00edeeb20e27b97868ba6
tal shachar <tal.shachar@buzzfeed.com>
2017-11-23 04:23:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,16 @@
 //define functions here
 
+function getIt () {
+  alert("Hey!")
+    return
+}
+
+
 $(document).ready(function(){
 
 // call functions here
+
+$('p').click("getIt", function(){
+
 
 });
